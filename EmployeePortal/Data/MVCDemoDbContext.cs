8f540c862f8d48138a376c9eb813bcb369ed17f6
Ariyanayagan T <ariyanayagan@cygnussoftwares.com>
2023-10-31 09:18:40 +0530

    Add project files.

---
+++ EmployeePortal/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeePortal.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
