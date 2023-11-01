0891a82b0062f1aea425f868012f9f6562a22da0
Ariyanayagan T <ariyanayagan@cygnussoftwares.com>
2023-11-01 17:52:56 +0530

    Changes

---
+++ EmployeePortal/Controllers/EmployeesController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    public class EmployeesController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
