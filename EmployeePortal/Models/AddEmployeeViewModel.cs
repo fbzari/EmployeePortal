a5fc117220972dc22fb5a6b1f0bb380aeacb1096
Ariyanayagan T <ariyanayagan@cygnussoftwares.com>
2023-11-04 16:00:34 +0530

    Changes -Delete - Finished

--- EmployeePortal/Models/AddEmployeeViewModel.cs
+++ EmployeePortal/Models/AddEmployeeViewModel.cs
@@ -2,10 +2,10 @@
 {
     public class AddEmployeeViewModel
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public long Salary { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Department { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public long Salary { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string Department { get; set; }
     }
 }
