b5228ee959da65982e8e1b1ec612f447db4b2f3e
Ariyanayagan <ariyanayagan@cygnussoftwares.com>
2024-07-31 11:09:33 +0530

    Before Docker

---
+++ EmployeePortal/Controllers/CardController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeePortal.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("Card")]
+    public class CardController : Controller
+    {
+        public readonly MVCDemoDbContext mvcDemoDbContext;
+        public CardController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
+        [Route("Show")]
+        public async Task<ActionResult> cards()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        public async Task<ActionResult> cardsDelete(Guid id)
+        {
+            var employee = await mvcDemoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee != null)
+            {
+                mvcDemoDbContext.Employees.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("cards");
+        }
+    }
+}
